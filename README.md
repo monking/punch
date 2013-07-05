fdc617b1cb058cfd5e8aaa37acdfe8054c7cc2b4
Christopher Lovejoy <c@lovejoy.info>
2013-07-05 13:16:53 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,157 @@
+punch
+=====
+
+Bash script to track time on projects in CSV files.
+
+Installation
+------------
+
+Copy the following into your `~/.bash_rc` file (or `~/.bash_profile`, as the
+case may be), and modify to suite your file structure:
+
+	export PUNCHDIR="$HOME/punch"
+	export TIMECLOCKDIR="$HOME/timeclock"
+	export CLIENTSDIR="$HOME/clients"
+	export REMOTETIMECLOCKDIR="username@remote.server:punch"
+	source $PUNCHDIR/punch.sh
+
+Configure these variables so that:
+
+- `PUNCHDIR` is the path to the directory where this script lies.
+- `TIMECLOCKDIR` is the path to the directory where your logs will be
+  stored.
+- `CLIENTSDIR` is the path to the directory where links to your project
+  directories will be stored.
+- `REMOTETIMECLOCKDIR` is a scp-compatible path to remote copy of
+  `TIMECLOCKDIR`.
+
+_Note:_ One feature of the punch script is to help you quickly navigate to your
+projects' working directories. In order do this punch.sh must be sourced rather
+than run, so that it is defined in your active shell session, not inside a
+script session.
+
+Usage
+-----
+
+The script defines many aliases for itself. The most commonly used are:
+- `p <task>`to switch to a task
+- `p [out|break|lunch]` to punch out quickly with a mildly descriptive message
+- `pgr` to return to the directory of the current project
+- `pr` to see what your current task is, when it was started, and how long
+  you've been on it
+- `pk` to see a summary of time spent today
+- `pkv` to see the time spent on all tasks today
+- `pd|pu` to download/upload at the beginning/end of the day
+- `p -Scf Monday -t Friday` to generate a daily summary for a week with one client
+
+Typing just `punch` in the command-line will give you the function's manpage,
+copied here:
+
+    NAME
+           punch - Record and calculate timeclock entries
+    
+    SYNOPSIS
+           punch [-c | -C <client>] [j | -J <project>] [-t <time> | -i | -o]
+                 [-s | -S] [-f <time> | -p | -h | -k] [-wr] [-vq | -m <format>]
+				 [-du] [-elg] [<action> | -a]
+    
+    OPTIONS
+           -a
+               Resume the last action, not counting breaks.
+    
+           -c
+			   Specify a client. If no other argument supplies the client name,
+		   then you will be prompted to enter the name. Previously entered
+		   names can be auto-completed using the <TAB> key.
+    
+           -C <client>
+               Specify the client name explicitly.
+    
+           -d
+			   Download the latest punch file from the server. OVERWRITES
+		   WITHOUT CHECKING
+    
+           -e
+			   Open the timeclock file in VIM. Which timeclock file is opened
+		   depends on the value of -t.
+    
+           -f <time>
+			   Set the time from which to start. This can be in any format that
+		   GNU date can parse. The default is the first of day  of the month
+		   specified by -t.
+    
+           -g
+			   Go to the directory linked to a project. (non-functional as a
+		   script. Save this as a function to use this feature)
+    
+           -h <date>
+			   Supply  a  date  to  get  the  hours  worked  on  that  day.
+		   This is equivalent to using -s -f "MM/DD/YYYY 00:00:00" -t
+		   "MM/DD/YYYY 23:59:59", where "MM/DD/YYYY" is the given date, in any
+		   format. Implies -s.
+    
+           -i
+               Use / show the last IN record in the timeclock.
+    
+           -j
+			   Enter the project at a prompt, with previous project records
+		   available for  autocomplete  by  pressing  <TAB>.  This  is enabled
+		   by default when recording an entry in the timeclock.
+    
+           -J <project>
+               Specify the project name explicitly.
+    
+           -k
+			   Set the beginning of the time range to midnight today.  Implies
+		   -s, overrides -f.
+    
+           -l
+			   Create a link to the current working directory under the given
+		   client and project.
+    
+           -m
+			   Specify the output format: "default", "minimal", or
+		   "spreadsheet".
+    
+           -o
+               Use / show the last OUT record in the timeclock.
+    
+           -p
+			   Use the time stored in the client record as the beginning of the
+		   time range (see -w). Implies -s, overrides -f.
+    
+           -q
+			   Do not show percent progress. Use this option when piping
+		   stdout.
+    
+           -r
+               Use / show the last record in the timeclock.
+    
+           -s
+			   Calculate the sum of all the records in a given range, from a
+		   specific client if provided.
+    
+           -S
+			   Calculate  daily sums of all the records in a given range, from
+		   a specific client if provided. Implies "minimal" format.
+    
+           -t <time>
+			   Set the current time for a new timeclock entry, or when summing,
+		   set the end of the time range. This can be in any  format that GNU
+		   date can parse. The default is "now".
+    
+           -u
+			   Upload the latest punch file to the server. OVERWRITES WITHOUT
+		   CHECKING
+    
+           -v
+			   Show a verbose sum, with totals by task, not just by project.
+		   Implies -s.
+    
+           -w
+			   Write the beginning of the time range to the client record
+		   (see -p).
+    
+    BUGS
+		   If a block of time crosses midnight, it is counted in the day in
+	   which it begins, rather than being divided at midnight.
---
+++ punch.sh
@@ -0,0 +1,520 @@
+#---------------------------------------------------------#
+#                   NUMBER FORMATTING                     #
+#---------------------------------------------------------#
+function pad {
+	local len str pad padAmt 
+	if [ -n "$1" ]; then
+		len=$1
+	else
+		return 0
+	fi
+	if [ -n "$2" ]; then
+		str="$2"
+	else
+		str=""
+	fi
+	if [ -n "$3" ]; then
+		pad="$3"
+	else
+		pad=" "
+	fi
+	padAmt=$(($len - ${#str}))
+	if [ $padAmt -gt 0 ]; then
+		for i in $(eval echo \{1..$padAmt\}); do
+			echo -n "$pad"
+		done
+	fi
+	echo -n $str
+}
+function formatSeconds {
+	local dayPlural days formatted granularity hours minutes maxScale scale seconds unit unitPlural
+	if [ -z "$1" ]; then
+		return 0
+	fi
+	if [ -n "$2" ]; then
+		unit="$2"
+	else
+		unit="seconds"
+	fi
+	case "$3" in
+		seconds) maxScale=1;;
+		minutes) maxScale=2;;
+		hours) maxScale=3;;
+		*) maxScale=4;;
+	esac
+	if [ $unit = "days" ]; then
+		granularity=4
+	elif [ $unit = "hours" ]; then
+		granularity=3
+	elif [ $unit = "minutes" ]; then
+		granularity=2
+	else
+		granularity=1
+	fi
+
+	seconds=0
+	minutes=0
+	hours=0
+	days=0
+	if [ $maxScale -gt 1 ]; then
+		seconds=$(($1 % 60))
+	elif [ $maxScale -eq 1 ]; then
+		seconds=$1
+	fi
+	if [ $maxScale -gt 2 -a $1 -gt 60 ]; then
+		minutes=$(($1 / 60 % 60))
+	elif [ $maxScale -eq 2 ]; then
+		minutes=$(($1 / 60))
+	fi
+	if [ $maxScale -gt 3 -a $1 -gt 3600 ]; then
+		hours=$(($1 / 3600 % 24))
+	elif [ $maxScale -eq 3 ]; then
+		hours=$(($1 / 3600))
+	fi
+	if [ $maxScale -ge 4 ]; then
+		days=$(($1 / 86400))
+	fi
+
+	unitPlural="$unit"
+	if [ "$(eval "echo \$$unit")" -eq 1 ]; then
+		unitPlural="$(echo $unitPlural | sed 's/s$//')"
+	fi
+
+	if [ $days -gt 0 ]; then
+		scale=4
+	elif [ $hours -gt 0 ]; then
+		scale=3
+	elif [ $minutes -gt 0 ]; then
+		scale=2
+	else
+		scale=1
+	fi
+	
+	if [ $granularity -gt $scale ]; then
+		# echo -n "0 $unitPlural"
+		echo -n "0:00"
+		return 0
+	fi
+
+	formatted=""
+	if [ $scale -ge 4 -a $granularity -le 4 ]; then
+		if [ $days -ne 1 ]; then
+			dayPlural="s"
+		fi
+		formatted="$formatted $days day$dayPlural"
+	fi
+	if [ $scale -ge 2 -a $granularity -le 3 ]; then
+		if [ -n "$formatted" ]; then
+			formatted="$formatted "
+		fi
+		formatted="$formatted$hours"
+	fi
+	if [ $scale -ge 2 -a $granularity -le 2 ]; then
+		if [ -n "$formatted" ]; then
+			formatted="$formatted:"
+		fi
+		formatted="$formatted$(pad 2 $minutes 0)"
+	fi
+	if [ $scale -ge 1 -a $granularity -le 1 ]; then
+		if [ -n "$formatted" ]; then
+			formatted="$formatted:"
+		fi
+		formatted="$formatted$(pad 2 $seconds 0)"
+	fi
+	echo -n "$formatted"
+}
+
+#---------------------------------------------------------#
+#                          PUNCH                          #
+#---------------------------------------------------------#
+# indent all with tab character, run, repeat until only vars remain on
+# their own lines:
+#     :%s /.\{-\}\(\w\+\)=\(.*\)/\1\r\t\2/
+# then `sort -u` to make `local` list
+function punch {
+	local pa pd pb pY pT pZ T Y Z a action b bold client clientDefault clientMarker clientReq d dailySum dailySumFrom date dayCount dayMax doit dosum download fUTime from fromPaid goToDir io latestfile line makeLink month normal oneDay other pAction pClient pDate pInOut pProject pUTime project projectDefault projectReq quiet readLog resumeLastIn format to today upload uTime verbose wd wdmarker writeFile writePaid year
+	while getopts "cC:jJ:t:sSf:ph:kwedvlgrioam:qu" flag
+	do
+		case $flag in
+			a ) resumeLastIn=y;readLog=lastInLine;clientReq=y;projectReq=y;;
+			c ) clientReq=y;;
+			C ) client="$OPTARG";;
+			d ) download=y;;
+			e ) readLog=whole;;
+			f ) from="$OPTARG";;
+			g ) goToDir=y;clientReq=y;projectReq=y;;
+			h ) oneDay="$OPTARG";dosum=y;;
+			i ) readLog=lastInLine;;
+			j ) projectReq=y;clientReq=y;;
+			J ) project="$OPTARG";;
+			k ) today=y;dosum=y;;
+			l ) makeLink=y;clientReq=y;projectReq=y;;
+			m ) format="$OPTARG";;
+			o ) readLog=lastOutLine;;
+			p ) fromPaid=y;clientReq=y;dosum=y;;
+			q ) quiet=y;;
+			r ) readLog=lastLine;;
+			s ) dosum=y;;
+			S ) dosum=y;dailySum=y;format=minimal;;
+			t ) to="$OPTARG";;
+			u ) upload=y;;
+			v ) verbose=y;dosum=y;;
+			w ) writePaid=y;clientReq=y;;
+		esac
+	done
+	if [ "$(echo $format | perl -pe 's/default|minimal|spreadsheet/y/')" != y ]; then
+		format=default
+	fi
+	case "$format" in
+		default ) ;;
+		minimal ) ;;
+		spreadsheet ) quiet=y;;
+	esac
+	shift $((OPTIND-1)); OPTIND=1
+	wd="$PWD"
+	action="$(echo $@ | sed 's/^\s+|\s+$|\r|\n//g')" 
+	if [ -z "$action" -a -z "$readLog" -a -z "$upload" -a -z "$download" -a "$dosum" != y -a "$goToDir" != y -a "$writePaid" != y -a "$makeLink" != y ]; then
+		bold=$(tput bold)
+		normal=$(tput sgr0)
+		# echo "see the manpage for usage info. (man punch)"
+		man $PUNCHDIR/.punch.1.gz
+		return 0
+	fi
+	if [ -z "$to" ]; then
+		if [ -n "$oneDay" ]; then
+			to="$oneDay 23:59:59"
+		else
+			to="now"
+		fi
+	fi
+	if [ -n "$(command -v gdate)" ]; then
+		read year month uTime date <<< $(gdate --date="$to" "+%Y %m %s %a_%b_%d_%T_%Z_%Y")
+	else
+		read year month uTime date <<< $(date --date="$to" "+%Y %m %s %a_%b_%d_%T_%Z_%Y")
+	fi
+	date="${date//_/ }"
+	if [ -z "$year" -o -z "$month" ]; then
+		echo "ERROR: invalid time"
+		return 0
+	fi
+	writeFile="$TIMECLOCKDIR/workclock_${year}_${month}.csv"
+	read latestfile other <<< $(ls -r1 $TIMECLOCKDIR/workclock_*.csv)
+	if [ -n "$latestfile" ]; then
+		if [ "$upload" = y ]; then
+			scp "$latestfile" "$REMOTETIMECLOCKDIR/"
+			return 0
+		fi
+		if [ "$download" = y ]; then
+			scp "$REMOTETIMECLOCKDIR/`basename $latestfile`" "$latestfile"
+			return 0
+		fi
+		if [ "$readLog" = lastInLine ]; then
+			while read -e line; do
+				if [ "${line/\"*\", \"*\", \"i\", */y}" = y ]; then
+					read pUTime pa pb pd pT pZ pY pInOut pClient pProject pAction <<< "$(echo "$line" | perl -pe 's/"([^"]+)",?/$1/g')"
+					continue
+				fi
+			done <<< "$(tail -2 "$latestfile")"
+		elif [ "$readLog" = lastOutLine ]; then
+			while read -e line; do
+				if [ "${line/\"*\", \"*\", \"o\", */y}" = y ]; then
+					read pUTime pa pb pd pT pZ pY pInOut pClient pProject pAction <<< "$(echo "$line" | perl -pe 's/"([^"]+)",?/$1/g')"
+					continue
+				fi
+			done <<< "$(tail -10 "$latestfile")"
+		else
+			read pUTime pa pb pd pT pZ pY pInOut pClient pProject pAction <<< "$(tail -1 "$latestfile" | perl -pe 's/"([^"]+)",?/$1/g')"
+		fi
+		if [ "$resumeLastIn" = y ]; then action="$pAction"; fi
+		pDate="$pa $pd $pb $pY $pT $pZ"
+	fi
+	if [ ! -a "$CLIENTSDIR" ]; then
+		mkdir -p "$CLIENTSDIR"
+	fi
+	if [ -n "$action" ]; then
+		clientReq=y
+		projectReq=y
+		if [ "$(echo "$action" | perl -pe 's/^(stop|out|break|lunch|done)$/y/')" = y ]; then
+			readLog=lastLine
+			# project=general
+			io=o
+		else
+			io=i
+		fi
+	fi
+	if [ -z "$client" -a "$clientReq" == y ]; then
+		if [ -n "$pClient" -a "${readLog/In/}" = lastLine ]; then
+			client=$pClient
+		else
+			if [ -n "$pClient" ]; then
+				clientDefault=" [$pClient]"
+			fi
+			printf "client$clientDefault: "
+			cd "$CLIENTSDIR/"
+			read -e client
+			cd "$wd"
+			if [ -z "$client" ]; then
+				client="$pClient"
+			fi
+		fi
+	fi
+	client="$(echo $client | perl -pe 's/\/|\n|\r//g' | perl -pe 's/[^\w]+/_/g')"
+	clientMarker="$CLIENTSDIR/$client"
+	if [ ! -d "$clientMarker" ]; then
+		mkdir "$clientMarker"
+	fi
+	if [ "$writePaid" = y ]; then
+		echo "$from" > "$clientMarker/.paid"
+	fi
+	if [ -z "$project" -a "$projectReq" == y ]; then
+		if [ -n "$pProject" -a "${readLog/In/}" = lastLine ]; then
+			project=$pProject
+		else
+			if [ -n "$pProject" ]; then
+				projectDefault=" [$pProject]"
+			fi
+			printf "project$projectDefault: "
+			cd "$clientMarker"
+			read -e project
+			cd "$wd"
+			if [ -z "$project" ]; then
+				project="$pProject"
+			fi
+		fi
+	fi
+	project="$(echo $project | perl -pe 's/\/|\n|\r//g' | perl -pe 's/[^\w]+/_/g')"
+	if [ ! -d "$clientMarker/$project" ]; then
+		mkdir -p "$clientMarker/$project"
+	fi
+	wdmarker="$clientMarker/$project/.working_directory"
+	if [ "$makeLink" = y ]; then
+		if [ -a "$wdmarker" ]; then
+			unlink "$wdmarker"
+		fi
+		ln -s "$wd" "$wdmarker"
+		echo "link established"
+	fi
+	if [ "$goToDir" = y ]; then
+		cd "$(readlink "$wdmarker")"
+	fi
+	if [ -n "$action" ]; then
+		echo "$client -- $project   $action   ($date)"
+		echo "\"$uTime\", \"$date\", \"$io\", \"$client\", \"$project\", \"$action\"" >> "$writeFile"
+	elif [ "$dosum" = y ]; then
+		function punchsum {
+			local T Y Z a actionSum actionTitle actions b clientTitle d date fDate fMonth fSDate fUTime fYear fYMD hours hoursTitle lastAction lastProject lastUTime line lineAction lineClient lineIO lineProject lineUTime maxClLen maxPrLen minutes month numLines onLine period projectSum projectTitle projects readFile readMonth readYear sum sumFrom sumProject sumTo uTime year
+			sumFrom="$1"
+			sumTo="$2"
+			if [ -z "$quiet" -a "$(uname | perl -pe 's/.*CYGWIN.*/CYGWIN/i')" = "CYGWIN" ]; then
+				echo "WARNING: Cygwin calculates very slowly."
+			fi
+			if [ -n "$(command -v gdate)" ]; then
+				read fYear fMonth fUTime fDate fSDate fYMD <<< $(gdate --date="$sumFrom" "+%Y %m %s %a_%b_%d_%T_%Z_%Y %a_%m/%d %Y/%m/%d")
+				read year month uTime date <<< $(gdate --date="$sumTo" "+%Y %m %s %a_%b_%d_%T_%Z_%Y")
+			else
+				read fYear fMonth fUTime fDate fSDate fYMD <<< $(date --date="$sumFrom" "+%Y %m %s %a_%b_%d_%T_%Z_%Y %a_%m/%d %Y/%m/%d")
+				read year month uTime date <<< $(date --date="$sumTo" "+%Y %m %s %a_%b_%d_%T_%Z_%Y")
+			fi
+			fDate="${fDate//_/ }"
+			fSDate="${fSDate//_/ }"
+			date="${date//_/ }"
+			hoursTitle="HOURS"
+			clientTitle="CLIENT"
+			projectTitle="PROJECT"
+			actionTitle="ACTION"
+			sum=0; lastUTime=0; projects=""; actions=""; readYear=$fYear; readMonth=$fMonth; maxPrLen=${#projectTitle}; maxClLen=${#clientTitle};
+			while [ $readYear$readMonth -le $year$month ]; do
+				readFile="$TIMECLOCKDIR/workclock_${readYear}_${readMonth}.csv"
+				if [ -r "$readFile" ]; then
+					numLines=$(grep -c '^' "$readFile")
+					onLine=0
+					while read -e line; do
+						if [ -z "$line" ]; then continue; fi
+						onLine=$(($onLine + 1))
+						if [ -z "$quiet" ]; then
+							echo -ne "  computing $readYear/$readMonth ($numLines entries) -- $((${onLine}*100/${numLines}))%\r"
+						fi
+						read lineUTime a d b Y T Z lineIO lineClient lineProject lineAction <<< "$( echo $line | perl -pe 's/"([^"]+)",?/$1/g')"
+						if [ "$lastUTime" -ne 0 ]; then
+							period="$(expr $lineUTime - $lastUTime)"
+							if [ -z "$(eval "echo \$sum_$lastProject")" ]; then
+								projects="$projects $lastProject"
+							fi
+							eval "local sum_$lastProject=\"\$(expr 0\$sum_$lastProject + $period)\""
+							if [ -z "$(eval "echo \$sum_$lastAction")" ]; then
+								actions="$actions $lastAction"
+							fi
+							eval "local sum_$lastAction=\"\$(expr 0\$sum_$lastAction + $period)\""
+							sum="$(expr $sum + $period)"
+							lastUTime=0
+						fi
+						if [ $lineUTime -gt $fUTime -a $lineUTime -lt $uTime ]; then
+							if [ "$lineIO" = "i" -a \( -z "$client" -o "$lineClient" = "$client" \) -a \( -z "$project" -o "$lineProject" = "$project" \) ]; then
+								if [ ${#lineClient} -gt $maxClLen ]; then maxClLen=${#lineClient}; fi
+								if [ ${#lineProject} -gt $maxPrLen ]; then maxPrLen=${#lineProject}; fi
+								lastProject="${lineClient}___$(echo "$lineProject" | perl -pe 's/[^a-zA-Z0-9\n\r]+/_/g')"
+								lastAction="${lastProject}__$(echo "$lineAction" | perl -pe 's/[^a-zA-Z0-9\n\r]+/_/g')"
+								lastUTime=$lineUTime
+							else 
+								lastUTime=0
+								lastProject=""
+								lastAction=""
+							fi
+						fi
+					done < "$readFile"
+				fi
+				if [ -a "$(command -v gdate)" ]; then
+					read readYear readMonth <<< $(gdate --date="$readMonth/1/$readYear +1 month" "+%Y %m")
+				else
+					read readYear readMonth <<< $(date --date="$readMonth/1/$readYear +1 month" "+%Y %m")
+				fi
+			done
+			if [ -z "$quiet" ]; then
+				echo -ne "										 \r"
+			fi
+			if [ "$lastUTime" -ne 0 ]; then
+				period=$(($uTime - $lastUTime))
+				if [ -z "$(eval "echo \$sum_$lastProject")" ]; then
+					projects="$projects $lastProject"
+				fi
+				eval "local sum_$lastProject=\"\$(expr 0\$sum_$lastProject + $period)\""
+				if [ -z "$(eval "echo \$sum_$lastAction")" ]; then
+					actions="$actions $lastAction"
+				fi
+				eval "local sum_$lastAction=\"\$(expr 0\$sum_$lastAction + $period)\""
+				sum="$(expr $sum + $period)"
+			fi
+			if [ $sum -eq 0 ]; then
+				return 0
+			fi
+			case "$format" in
+				minimal ) echo -e "$fSDate : $(formatSeconds $sum minutes hours)";;
+				spreadsheet ) ;;
+				* ) echo -e "$fDate (from)\n$date ('til)\n";;
+			esac
+			if [ "$verbose" = y ]; then
+				if [ "$format" == "default" ]; then
+					if [ -z "$client" ]; then
+						echo "$(pad $((7 - ${#hoursTitle})))$hoursTitle  $(pad $(($maxClLen - ${#clientTitle})))$clientTitle  $(pad $(($maxPrLen - ${#projectTitle})))$projectTitle  $actionTitle"
+					else
+						echo "$(pad $((7 - ${#hoursTitle})))$hoursTitle  $(pad $(($maxPrLen - ${#projectTitle})))$projectTitle  $actionTitle"
+					fi
+				fi
+				for action in $actions; do
+					eval "actionSum=\$sum_$action"
+					read lineClient lineProject lineAction <<< $(echo $action | perl -pe 's/__+/ /g')
+					hours="$(formatSeconds $actionSum minutes hours)"
+					if [ -z "$client" ]; then
+						case "$format" in
+							spreadsheet ) echo "$fYMD	$hours	$lineClient	$lineProject	$lineAction";;
+							* ) echo "$(pad $((7 - ${#hours})))$hours  $(pad $(($maxClLen - ${#lineClient})))$lineClient  $(pad $(($maxPrLen - ${#lineProject})))$lineProject  ${lineAction//_/ }";;
+						esac
+					else
+						case "$format" in
+							spreadsheet ) echo "$fYMD	$hours	$lineProject	$lineAction";;
+							* ) echo "$(pad $((7 - ${#hours})))$hours  $(pad $(($maxPrLen - ${#lineProject})))$lineProject  ${lineAction//_/ }";;
+						esac
+					fi
+				done
+				case "$format" in 
+					spreadsheet ) ;;
+					* ) echo "----------------------------------------------------";;
+				esac
+			else
+				if [ "$format" == "default" ]; then
+					if [ -z "$client" ]; then
+						echo "$(pad $((7 - ${#hoursTitle})))$hoursTitle  $clientTitle$(pad $(($maxClLen - ${#clientTitle})))  $projectTitle"
+					else
+						echo "$(pad $((7 - ${#hoursTitle})))$hoursTitle  $projectTitle"
+					fi
+				fi
+				for sumProject in $projects; do
+					eval "projectSum=\$sum_$sumProject"
+					read lineClient lineProject <<< $(echo $sumProject | perl -pe 's/__+/ /g')
+					hours="$(formatSeconds $projectSum minutes hours)"
+					if [ -z "$client" ]; then
+						case "$format" in
+							spreadsheet ) echo "$fYMD	$hours	$lineClient	$lineProject";;
+							* ) echo "$(pad $((7 - ${#hours})))$hours  $lineClient$(pad $(($maxClLen - ${#lineClient})))  $lineProject";;
+						esac
+					else
+						case "$format" in
+							spreadsheet ) echo "$fYMD	$hours	$lineProject";;
+							* ) echo "$(pad $((7 - ${#hours})))$hours  $lineProject";;
+						esac
+					fi
+				done
+			fi
+			if [ "$format" == "default" ]; then
+				echo "hours: $(formatSeconds $sum minutes hours)"
+			fi
+		}
+		if [ -n "$oneDay" ]; then
+			from="$oneDay 00:00:00"
+		elif [ "$fromPaid" = y ]; then
+			from="$(cat "$clientMarker/.paid")"
+		elif [ "$today" = y ]; then
+			from="today 0:00"
+		fi
+		if [ -z "$from" ]; then
+			from="$month/1/$year"
+		fi
+		if [ "$dailySum" = y ]; then
+			if [ -a "$(command -v gdate)" ]; then
+				fUTime=$(gdate --date="$from" "+%s")
+			else
+				fUTime=$(date --date="$from" "+%s")
+			fi
+			dayMax=$(((uTime - fUTime) / 86400))
+			if [ $(((uTime - fUTime) % 86400)) -ne 0 ]; then
+				dayMax=$((dayMax+1))
+			fi
+			dayCount=0
+			dailySumFrom="$(echo $from | perl -pe 's/ \d:\d+(:\d+)//')"
+			if [ "$format" == "default" ]; then
+				echo "----------------------------------------------------"
+			fi
+			while [ $dayCount -lt $dayMax ]; do
+				punchsum "$dailySumFrom +${dayCount}days" "$dailySumFrom +$(($dayCount+1))days"
+				dayCount=$((dayCount+1))
+			done
+		else
+			punchsum "$from" "$to"
+		fi
+	elif [ "$readLog" = "whole" ]; then
+		if [ -r "$writeFile" ]; then
+			vim "$writeFile"
+		else
+			vim "$latestfile"
+		fi
+		return 0
+	elif [ -n "$readLog" ]; then
+		echo "$pClient -- $pProject   $pAction   $(echo $pT | perl -pe 's/:\d+$//') ($(formatSeconds $(($uTime - $pUTime)) minutes hours))"
+		return 0
+	fi
+}
+
+#---------------------------------------------------------#
+#                       ALIASES                           #
+#---------------------------------------------------------#
+alias p="punch"
+alias pl="punch -l"
+alias plr="punch -lr"
+alias pg="punch -g"
+alias pgr="punch -gr"
+alias pp="punch -p"
+alias ppv="punch -pv"
+alias pw="punch -wf"
+alias pk="punch -k"
+alias pkv="punch -kv"
+alias ph="punch -h"
+alias phv="punch -vh"
+alias pt="punch -t"
+alias pr="punch -r"
+alias pe="punch -e"
+alias pi="punch -i"
+alias po="punch -o"
+alias psf="p -Sf \"0:00:00 last Monday\" -t \"0:00:00 last Monday +7days\" -C $(basename $(canon "$CLIENTSDIR/full-time"))"
+alias pin="punch -ai"
+alias pu="punch -u"
+alias pd="punch -d"
